51c0102c08d4179d661386663f71cda253d59e6d
Button99 <christoskoubis@yahoo.com>
2021-09-28 20:02:03 +0300

    Vue v1

---
+++ MyCar/resources/js/components/Vehicles.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1> Vehicles </h1>
+</template>
+
+<script>
+    export default {
+        data: function() {
+        },
+        mounted: function() {
+            console.log('Vehicles comp loaded');
+        }
+    }
+</script>
--- MyCar/routes/api.php
+++ MyCar/routes/api.php
@@ -2,17 +2,35 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\DataLog;
+use App\Models\User;
+use App\Models\Vehicle;
+use App\Models\Picture;
+use App\Models\Accessory;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group(['middleware' => 'api'], function() {
+    Route::get('vehicles', function() {
+        return Vehicle::latest()->orderBy('created_at', 'desc')->get();
+    });
+
+    Route::get('vehicles/{id}', function($id) {
+        return Vehicle::findOrFail($id);
+    });
+
+//     Route::post('vehicles/store', function(Request $req) {
+//         return Vehicle::create(['fiels...']);
+//     });
+// });
+
+    // Route::patch('vehicle/{id}', function(Request $req, $id) {
+    //     Vehicle::findOrFail($id)->update('fields...');
+        
+    // });
+
+    Route::delete('vehicle/{id}', function($id) {
+        return Vehicle::destroy($id);
+    });
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
